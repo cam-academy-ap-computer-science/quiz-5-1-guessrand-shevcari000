9d405523133b032143f0990c5d7c24ea46055ada
shevcari000 <shevcari000@CAM-LAB-29.bgsd.k12.wa.us>
2019-11-15 07:47:03 -0800

    finished.

--- src/GuessRand.java
+++ src/GuessRand.java
@@ -4,25 +4,23 @@ public class GuessRand {
 	public static void main(String[] args) {
 		int rand = 0;
 		boolean success = false;
-		Random r = new Random
-		result = r.nextInt(101 + 1);
-		while (    // loop until 'success' is true. ) {
+		Random r = new Random();
+		rand = r.nextInt(100) + 1;
+		while (success != true) {
 			int guess = 0;
-			// Instantiate a new Scanner 'console'
+			Scanner console = new Scanner(System.in);
 			System.out.println("Your Guess: ?");
-			guess = // Get an integer from the user.
-			success =  // call 'compare()' method
+			guess = console.nextInt();
+			success =  compare(guess, rand);
 		}
 	}
 	
-	// Declare a method named 'compare()' that accepts two parameters 'guess' and 'rand' and returns
-	// a boolean.
-	public static   
+	public static boolean compare (int guess, int rand) {   
 		boolean got_it = false;
-		if () {  // Fix if test
+		if (guess == rand) {  
 			System.out.println("You got it!");
 			got_it = true;
-		} else if () {  // Fix if test
+		} else if (guess < rand) {  
 			System.out.println("No, it's Higher");
 		} else {
 			System.out.println("No, it's Lower");
